151fa9002fdbd114fa0f5708ac767e00ab8c1dcb
89258085058 <86528556+89258085058@users.noreply.github.com>
2022-05-16 22:51:46 +0300

    Добавлена обработка информации о ковиде

--- covid.py
+++ covid.py
@@ -26,7 +26,9 @@ def mess(messege):
         location = covid19.getLocationByCountryCode("RU")
     else:
         location = covid19.getLatest()
-        final_message = f"<u>Данные по всему миру:</u>\n<b>Заболевшие: </b>{location['confirmed']}"
+        final_message = f"<u>Данные по всему миру:</u>\n" \
+                        f"<b>Заболевшие: </b>{location['confirmed']}\n" \
+                        f"<b>Умершие: </b>{location['deaths']}\n"
 
     if final_message == "":
         final_message = f"Данные по стране: \n" \
